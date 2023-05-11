b12792d209b1360782a141de469ee915363c8adb
Chase Medford <chasemedford1@gmail.com>
2023-05-10 23:59:40 -0500

    first commit

---
+++ src/test/java/com/chasemedford/Portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chasemedford.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
