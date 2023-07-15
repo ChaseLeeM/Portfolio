46a1757ddd33c6e7de1dc7205eb1d56ecbd6ca68
Chase Medford <chasemedford1@gmail.com>
2023-07-15 08:50:35 -0500

    Added a page that will allow me to upload files to my site.

---
+++ src/main/java/com/chasemedford/Portfolio/controllers/FileUploadController.java
@@ -0,0 +1,35 @@
+package com.chasemedford.Portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Controller
+@RequestMapping("/upload")
+public class FileUploadController {
+
+    @PostMapping
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        // Logic to handle file upload...
+
+        // If you need to save the file in a local directory
+        try {
+            Path path = Paths.get("uploads/" + file.getOriginalFilename());
+            Files.write(path, file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+}
